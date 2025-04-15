79acc9214d9e99cd9a0da8aa0abc32ed16e6a237
Chatali4738patil <152089851+Chatali4738patil@users.noreply.github.com>
2025-04-16 00:04:23 +0530

    add

---
+++ data/accounts.js
@@ -0,0 +1,7 @@
+let accounts = [
+    { id: 1, name: 'TechCorp', matchScore: 92, status: 'Not Target' },
+    { id: 2, name: 'InnovateX', matchScore: 78, status: 'Not Target' },
+    { id: 3, name: 'BizGroup', matchScore: 85, status: 'Target' }
+  ];
+  
+  module.exports = accounts;
